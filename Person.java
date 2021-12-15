128adc9ac2cb63f3d404f51e24227962f1694bf0
Marcelus (WHS) <mebsport@gmail.com>
2021-12-15 07:50:48 -0500

    Create Person.java
    created person class in the program

---
+++ Person.java
@@ -0,0 +1,24 @@
+
+/**
+ * Person outline for the user of the program.
+ *
+ * Marcelus Brown
+ * 12/15/21
+ */
+public class Person
+{
+    private String name;
+    private int age;
+    private int cash;
+    private boolean student;
+    
+    public Person (String n, int a, int c, boolean s)
+    {
+        name = n;
+        age = a;
+        cash = c;
+        student = s;
+    }
+    
+    
+}
