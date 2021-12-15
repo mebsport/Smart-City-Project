9c7d966d2846d5ef370e99a6a6e92bfca3e1436a
Marcelus (WHS) <mebsport@gmail.com>
2021-12-15 07:51:48 -0500

    Created a runner class in the program
    this class will go through and test all the other created classes.

---
+++ Runner.java
@@ -0,0 +1,14 @@
+
+/**
+ * runner for the program to go through and test everything
+ *
+ * Marcelus Brown
+ * 12/15/21
+ */
+public class Runner
+{
+    public static void main( String args[])
+    {
+        
+    }
+}
